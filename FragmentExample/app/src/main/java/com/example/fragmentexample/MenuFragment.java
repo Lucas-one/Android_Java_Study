5e41f54ea935422b02a17dfc4df67342f4625909
Lucas-one <first1373@naver.com>
2021-01-09 17:44:48 +0900

    FragmentExample_update

--- FragmentExample/app/src/main/java/com/example/fragmentexample/MenuFragment.java
+++ FragmentExample/app/src/main/java/com/example/fragmentexample/MenuFragment.java
@@ -1,26 +1,25 @@
 package com.example.fragmentexample;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class MainFragment extends Fragment {
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
         Button button = rootView.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MainActivity activity = (MainActivity) getActivity();
-                activity.onFragmentChanged(1);
+                activity.onFragmentChanged(0);
             }
         });
         return rootView;
